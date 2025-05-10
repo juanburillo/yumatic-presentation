3a064f4225edc6a5794954f58a052cc4fb8925c5
Juan <juanburillodev@gmail.com>
2025-05-10 21:52:58 +0200

    feat: change title arrangement

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,14 +1,17 @@
-import Countdown from "./Countdown.jsx"
+import Countdown from "./Countdown"
 import { RotatingWords } from "./RotatingWords.tsx"
 
 const Hero = () => {
   return (
     <div className="min-h-screen flex items-center justify-center">
       <main className="p-10 md:p-12 lg:p-16">
-        <h1 className="text-justify lg:px-20 text-5xl md:text-7xl lg:text-8xl font-display">
-          <span className="text-6xl md:text-8xl lg:text-9xl block sm:inline animate-gradient bg-gradient-to-r from-gradient-start via-gradient-mid to-gradient-end bg-clip-text text-transparent">
+        <h1 className="lg:px-24 text-5xl md:text-7xl lg:text-8xl font-display">
+          <span className="text-6xl md:text-8xl lg:text-9xl block animate-gradient bg-gradient-to-r from-gradient-start via-gradient-mid to-gradient-end bg-clip-text text-transparent">
             Yumatic
-          </span> is coming to change the way you&nbsp;
+          </span>
+          <span>
+            is coming to change the way you&nbsp;
+          </span>
           <RotatingWords words={["eat.", "cook.", "snack."]} />
         </h1>
 
