fba737ebf556ed8f3d03d2a72780e04ed6971e90
Juan <juanburillodev@gmail.com>
2025-05-10 21:56:24 +0200

    refactor: change padding for margin in title

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,7 +5,7 @@ const Hero = () => {
   return (
     <div className="min-h-screen flex items-center justify-center">
       <main className="p-10 md:p-12 lg:p-16">
-        <h1 className="lg:px-24 text-5xl md:text-7xl lg:text-8xl font-display">
+        <h1 className="lg:mx-24 text-5xl md:text-7xl lg:text-8xl font-display">
           <span className="text-6xl md:text-8xl lg:text-9xl block animate-gradient bg-gradient-to-r from-gradient-start via-gradient-mid to-gradient-end bg-clip-text text-transparent">
             Yumatic
           </span>
