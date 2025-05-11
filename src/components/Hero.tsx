854894c621d339ac88d274b427d5aeb14753fec9
Juan <juanburillodev@gmail.com>
2025-05-11 12:12:33 +0200

    style: order tailwind classes

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -4,15 +4,15 @@ import { RotatingWords } from "./RotatingWords"
 const Hero = () => {
   return (
     <div className="min-h-screen flex items-center justify-center">
-      <main className="p-10 md:p-12 lg:p-16">
-        <h1 className="lg:mx-24 text-5xl md:text-7xl lg:text-8xl font-display">
-          <span className="text-6xl md:text-8xl lg:text-9xl block animate-gradient bg-gradient-to-r from-gradient-start via-gradient-mid to-gradient-end bg-clip-text text-transparent">
+      <main className="p-10 md:p-12 lg:p-16 w-full">
+        <h1 className="lg:px-24 text-5xl md:text-7xl lg:text-8xl font-display">
+          <span className="block text-6xl md:text-8xl lg:text-9xl animate-gradient bg-gradient-to-r from-gradient-start via-gradient-mid to-gradient-end bg-clip-text text-transparent">
             Yumatic
           </span>
           <span>
             is coming to change the way you&nbsp;
+            <RotatingWords words={["eat.", "cook.", "snack."]} />
           </span>
-          <RotatingWords words={["eat.", "cook.", "snack."]} />
         </h1>
 
         <Countdown />
