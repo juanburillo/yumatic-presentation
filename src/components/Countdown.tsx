062051560ff40731b2e60172f4572bf09fd278f9
Juan <juanburillodev@gmail.com>
2025-05-11 20:11:42 +0200

    feat(seo): improve SEO in countdown

--- src/components/Countdown.tsx
+++ src/components/Countdown.tsx
@@ -1,8 +1,8 @@
 const Countdown = () => {
   return (
-    <div className="mt-12 md:mt-14 lg:mt-18 text-center">
+    <div className="mt-12 md:mt-14 text-center">
       <p className="text-7xl md:text-8xl lg:text-9xl font-light">
-        XX
+        <time dateTime="XX">XX</time>
       </p>
 
       <p className="text-xl sm:text-2xl md:text-3xl lg:text-4xl">
